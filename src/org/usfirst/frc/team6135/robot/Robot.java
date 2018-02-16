078f1f2697eba6ffda75a2b2e56e6bd351a27801
Tyler Tian <tylertian123@gmail.com>
2018-02-16 17:45:40 -0500

    Add experimental code for vision testing

--- src/org/usfirst/frc/team6135/robot/Robot.java
+++ src/org/usfirst/frc/team6135/robot/Robot.java
@@ -2,14 +2,12 @@
 package org.usfirst.frc.team6135.robot;
 
 import org.usfirst.frc.team6135.robot.subsystems.*;
+
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.usfirst.frc.team6135.robot.commands.autocommands.*;
-import org.usfirst.frc.team6135.robot.commands.autoutils.AutoTurn;
-import org.usfirst.frc.team6135.robot.commands.autoutils.DriveStraightDistance;
-
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
@@ -46,8 +44,8 @@ public class Robot extends IterativeRobot {
 	
 	public static UsbCamera camera;
 	public static Scalar redUpperBound1 = new Scalar(10, 255, 255);
-	public static Scalar redLowerBound1 = new Scalar(0, 210, 130);
-	public static Scalar redLowerBound2 = new Scalar(245, 210, 130);
+	public static Scalar redLowerBound1 = new Scalar(0, 210, 115);
+	public static Scalar redLowerBound2 = new Scalar(245, 210, 115);
 	public static Scalar redUpperBound2 = new Scalar(255, 255, 255);
 	public static Scalar blueUpperBound = new Scalar(170, 255, 255);
 	public static Scalar blueLowerBound = new Scalar(145, 190, 75);
@@ -61,7 +59,30 @@ public class Robot extends IterativeRobot {
 
 	Command autonomousCommand;
 	SendableChooser<Command> chooser = new SendableChooser<>();
+	
 
+	static class ByteArrayImg {
+		byte[] data;
+		final int width;
+		final int height;
+		
+		public ByteArrayImg(byte[] data, final int width, final int height) {
+			this.data = data;
+			this.width = width;
+			this.height = height;
+		}
+		public byte getPixelByte(int x, int y) {
+			return data[y * width + x];
+		}
+		public void setPixelByte(int x, int y, int colour) {
+			if(x < width && y < height && x >= 0 && y >= 0)
+				data[y * width + x] = (byte) colour;
+		}
+		public byte[] getBytes() {
+			return data;
+		}
+	}
+	
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
@@ -108,28 +129,89 @@ public class Robot extends IterativeRobot {
         		//Create a sink and a source
         		CvSink sink = CameraServer.getInstance().getVideo();
         		CvSource source = CameraServer.getInstance().putVideo("Test Stream", RobotMap.CAMFEED_WIDTH, RobotMap.CAMFEED_HEIGHT);
+        		CvSource noBlur = CameraServer.getInstance().putVideo("No Blur", RobotMap.CAMFEED_WIDTH, RobotMap.CAMFEED_HEIGHT);
         		//Create matrices to store the images later
         		Mat originalImg = new Mat();
         		Mat hsvImg = new Mat();
         		Mat filteredImg1 = new Mat();
         		Mat filteredImg2 = new Mat();
-        		Mat processedImg = new Mat();
+        		Mat filteredImg = new Mat();
+        		Mat buf = new Mat();
+        		//Mat processedImg = new Mat();
         		
         		while(!Thread.interrupted()) {
         			//Obtain the frame from the camera (1 second timeout)
         			sink.grabFrame(originalImg, 1);
+        			Imgproc.medianBlur(originalImg, buf, 3);
         			//Convert the colour space from BGR to HSV
-        			Imgproc.cvtColor(originalImg, hsvImg, Imgproc.COLOR_BGR2HSV_FULL);
+        			Imgproc.cvtColor(buf, hsvImg, Imgproc.COLOR_BGR2HSV_FULL);
         			//Filter out the colours
         			if(teamIsRed) {
         				Core.inRange(hsvImg, redLowerBound1, redUpperBound1, filteredImg1);
         				Core.inRange(hsvImg, redLowerBound2, redUpperBound2, filteredImg2);
-        				Core.addWeighted(filteredImg1, 1.0, filteredImg2, 1.0, 0.0, processedImg);
+        				Core.addWeighted(filteredImg1, 1.0, filteredImg2, 1.0, 0.0, filteredImg);
         			}
         			else {
-        				Core.inRange(hsvImg, blueLowerBound, blueUpperBound, processedImg);
+        				Core.inRange(hsvImg, blueLowerBound, blueUpperBound, filteredImg);
         			}
-        			source.putFrame(processedImg);
+        			
+        			noBlur.putFrame(filteredImg);
+        			
+        			byte[] imgData = new byte[(int) (filteredImg.total() * filteredImg.channels())];
+        			filteredImg.get(0, 0, imgData);
+        			byte[] processed = new byte[imgData.length];
+        			ByteArrayImg img = new ByteArrayImg(imgData, filteredImg.width(), filteredImg.height());
+        			ByteArrayImg imgOut = new ByteArrayImg(processed, filteredImg.width(), filteredImg.height());
+        			for(int y = 0; y < filteredImg.height(); y ++) {
+        				for(int x = 0; x < filteredImg.width(); x ++) {
+        					if(img.getPixelByte(x, y) != 0x00) {
+        						imgOut.setPixelByte(x - 1, y - 1, 0xFF);
+        						imgOut.setPixelByte(x, y - 1, 0xFF);
+        					}
+        				}
+        			}
+        			
+        			
+        			Mat processedMat = new Mat(filteredImg.height(), filteredImg.width(), filteredImg.type());
+        			processedMat.put(0, 0, processed);
+        			source.putFrame(processedMat);
+        			
+        			/*//Convert to BufferedImage
+        			MatOfByte mob = new MatOfByte();
+        			Imgcodecs.imencode(".jpg", filteredImg, mob);
+        			byte[] data = mob.toArray();
+        			//Do processing
+        			BufferedImage image;
+        			try {
+						image = ImageIO.read(new ByteArrayInputStream(data));
+					} catch (IOException e) {
+						e.printStackTrace();
+						continue;
+					}
+        			/*for(int y = 0; y < image.getHeight(); y ++) {
+        				for(int x = 0; x < image.getWidth(); x ++) {
+        					int rgb = image.getRGB(x, y);
+        					if(rgb == 0xFFFFFFFF)
+        						rgb = 0x00000000;
+        					else 
+        						rgb = 0xFFFFFFFF;
+        					image.setRGB(x, y, rgb);
+        				}
+        			}
+        			//Convert back to Mat
+        			Mat processedImg = new Mat(image.getWidth(), image.getHeight(), CvType.CV_8UC3);
+        			data = new byte[image.getWidth() * image.getHeight() * (int) processedImg.elemSize()];
+        			int[] imgDataSource = image.getRGB(0, 0, image.getWidth(), image.getHeight(), null, 0, image.getWidth());
+        			for(int i = 0; i < imgDataSource.length; i ++) {
+        				data[i * 3] = (byte) ((imgDataSource[i] >> 16) & 0xFF);
+        				data[i * 3 + 1] = (byte) ((imgDataSource[i] >> 8) & 0xFF);
+        				data[i * 3 + 2] = (byte) (imgDataSource[i] & 0xFF);
+        			}
+        			processedImg.put(0, 0, data);
+        			
+        			source.putFrame(processedImg);*/
+        			
+        			
         		}
         	}
         })).start();
