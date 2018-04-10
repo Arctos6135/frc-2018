d0d90e306f2d86139f20e616c71cfc39583a3470
TeamArctos6135 <programming.arctos@gmail.com>
2018-04-10 17:38:45 -0400

    Add a buffer zone in AutoPlayback and skip lines if playback is behind too much

--- src/org/usfirst/frc/team6135/robot/misc/AutoPlayback.java
+++ src/org/usfirst/frc/team6135/robot/misc/AutoPlayback.java
@@ -53,8 +53,8 @@ public class AutoPlayback {
 			//After waiting for how long do we have to run the line
 			double t_delta = currentLineTime - (System.currentTimeMillis() - startTime);
 			
-			//Run only if we are precisely on time or behind
-			if(t_delta <= 0) {
+			//If we are off by 10 milliseconds in both directions or less, run the line
+			if(Math.abs(t_delta) <= 10) {
 				//Set the motors one by one in the exact order we stored them
 				//Start at index 1 since index 0 is the time
 				RobotMap.leftDriveTalon1.set(ControlMode.PercentOutput, Double.parseDouble(currentLine[1]));
@@ -74,6 +74,30 @@ public class AutoPlayback {
 				else {
 					//Otherwise we've already gone though all lines and playback is finished
 					playbackFinished = true;
+				}
+			}
+			//Otherwise if we are behind for more than 10 milliseconds, skip it
+			else if(t_delta < -10) {
+				//Loop until we get to the correct time
+				while(true) {
+					//Move on to the next line
+					String line = reader.readLine();
+					
+					if(line != null) {
+						//If line is not null, set the value of currentLine and currentLineTime
+						currentLine = line.split(SEPARATOR);
+						currentLineTime = Long.parseLong(currentLine[0]);
+						//If with the new line we are not behind anymore, exit the loop
+						//Otherwise, get the next line and try again
+						if(currentLineTime - (System.currentTimeMillis() - startTime) >= 0) {
+							break;
+						}
+					}
+					else {
+						//Otherwise we've already gone though all lines and playback is finished
+						playbackFinished = true;
+						break;
+					}
 				}
 			}
 		}
