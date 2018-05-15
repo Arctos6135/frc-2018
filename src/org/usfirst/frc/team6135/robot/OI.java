446dc9217f6badd1248c27ea059f6398b0189949
TeamArctos6135 <programming.arctos@gmail.com>
2018-05-14 21:31:11 -0400

    Remove accidental introduction of scale position in demo mode

--- src/org/usfirst/frc/team6135/robot/OI.java
+++ src/org/usfirst/frc/team6135/robot/OI.java
@@ -234,7 +234,7 @@ public class OI {
 	
 	public static NonDemoButton recordAuto;
 	
-	public static JoystickButton scalePosition;
+	public static NonDemoButton scalePosition;
 	
 	public static NonDemoButton shootCube;
 	
@@ -256,7 +256,7 @@ public class OI {
 		cancelAlign = new NonDemoButton(driveController, Controls.CANCEL_ALIGN);
 		precisionToggle = new JoystickButton(driveController, Controls.PRECISION_TOGGLE);
 		recordAuto = new NonDemoButton(driveController, Controls.RECORD_AUTO);
-		scalePosition = new JoystickButton(attachmentsController, Controls.SCALE_POSITION);
+		scalePosition = new NonDemoButton(attachmentsController, Controls.SCALE_POSITION);
 		shootCube = new NonDemoButton(attachmentsController, Controls.SHOOT_CUBE);
 		
 		demo_blockAttachments = new DemoButton(driveController, Controls.DEMO_BLOCK_ATTACHMENTS, false);
