c9f3661c7ce61cf8ef466931562b92dcfd6f9fce
Tyler Tian <tylertian123@gmail.com>
2018-06-28 21:59:39 -0400

    Update code for RobotPathfinder v2.0.0-beta2

--- src/org/usfirst/frc/team6135/robot/misc/AutoPaths.java
+++ src/org/usfirst/frc/team6135/robot/misc/AutoPaths.java
@@ -6,8 +6,8 @@ import robot.pathfinder.core.RobotSpecs;
 import robot.pathfinder.core.TrajectoryParams;
 import robot.pathfinder.core.Waypoint;
 import robot.pathfinder.core.path.PathType;
-import robot.pathfinder.core.trajectory.BasicTrajectory;
 import robot.pathfinder.core.trajectory.TankDriveTrajectory;
+import robot.pathfinder.core.trajectory.TrajectoryGenerator;
 
 public class AutoPaths {
 	
@@ -29,36 +29,20 @@ public class AutoPaths {
 		params.pathType = PathType.QUINTIC_HERMITE;
 		params.segmentCount = 500;
 		
-		params.waypoints = new Waypoint[] {
-				new Waypoint(0, 0, Math.PI / 2),
-				new Waypoint(0, RobotMap.ArenaDimensions.BASELINE_DISTANCE, Math.PI / 2)
-		};
-		params.alpha = 1;
-		baseline_driveForward = new TankDriveTrajectory(new BasicTrajectory(specs, params));
-		
-		params.waypoints = new Waypoint[] {
-				new Waypoint(0, 0, Math.PI / 2),
-				new Waypoint(0, RobotMap.ArenaDimensions.SWITCH_DISTANCE - RobotMap.ROBOT_LENGTH, Math.PI / 2)
-		};
-		params.alpha = 1;
-		aligned_driveForward = new TankDriveTrajectory(new BasicTrajectory(specs, params));
+		baseline_driveForward = TrajectoryGenerator.generateStraightTank(specs, RobotMap.ArenaDimensions.BASELINE_DISTANCE);
+
+		aligned_driveForward = TrajectoryGenerator.generateStraightTank(specs, RobotMap.ArenaDimensions.SWITCH_DISTANCE - RobotMap.ROBOT_LENGTH);
 		
 		params.waypoints = new Waypoint[] {
 				new Waypoint(0, 0, Math.PI / 2),
 				new Waypoint(-RobotMap.ArenaDimensions.SWITCH_SIZE / 2, RobotMap.ArenaDimensions.SWITCH_DISTANCE - RobotMap.ROBOT_LENGTH, Math.PI / 2),
 		};
 		params.alpha = 125;
-		middle_left = new TankDriveTrajectory(new BasicTrajectory(specs, params));
+		middle_left = new TankDriveTrajectory(specs, params);
 		middle_right = middle_left.mirrorLeftRight();
 		middle_left2 = middle_left.retrace();
 		middle_right2 = middle_right.retrace();
-		
-		params.waypoints = new Waypoint[] {
-				new Waypoint(0, 0, Math.PI / 2),
-				new Waypoint(0, 60, Math.PI / 2),
-		};
-		params.alpha = 1;
-		middle_3 = new TankDriveTrajectory(new BasicTrajectory(specs, params));
+		middle_3 = TrajectoryGenerator.generateStraightTank(specs, 60);
 		middle_4 = middle_3.retrace();
 		
 		params.waypoints = new Waypoint[] {
@@ -66,7 +50,7 @@ public class AutoPaths {
 				new Waypoint(RobotMap.ArenaDimensions.SWITCH_EDGE_OFFSET, RobotMap.ArenaDimensions.SWITCH_CENTER_DISTANCE - RobotMap.ROBOT_LENGTH / 2, 0)
 		};
 		params.alpha = 100;
-		side_left = new TankDriveTrajectory(new BasicTrajectory(specs, params));
+		side_left = new TankDriveTrajectory(specs, params);
 		side_right = side_left.mirrorLeftRight();
 		
 		back_up = middle_4;
