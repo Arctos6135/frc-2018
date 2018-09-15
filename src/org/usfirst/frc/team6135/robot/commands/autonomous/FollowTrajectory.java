35bdb98ecd8722622696d443ff6fa856e9231d05
Tyler Tian <tylertian123@gmail.com>
2018-09-15 11:27:00 -0400

    Change FollowTrajectory command to use Follower class instead

--- src/org/usfirst/frc/team6135/robot/commands/autonomous/FollowTrajectory.java
+++ src/org/usfirst/frc/team6135/robot/commands/autonomous/FollowTrajectory.java
@@ -6,7 +6,8 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 import robot.pathfinder.core.trajectory.TankDriveTrajectory;
-import robot.pathfinder.core.trajectory.TankDriveMoment;
+import robot.pathfinder.follower.Follower;
+import robot.pathfinder.follower.TankFollower;
 
 /**
  * 	Makes the Robot follow the trajectory of a {@link robot.pathfinder.core.trajectory.TankDriveTrajectory TankDriveTrajectory}.<br>
@@ -22,71 +23,55 @@ public class FollowTrajectory extends Command {
 	//These are kept as non-constant to allow easy tuning from the SmartDashboard
 	public static double kA = 0.00215, kV = 0.01, kP = 0.02225, kD = 0.001;
 	
-	TankDriveTrajectory trajectory;
+	static final Follower.TimestampSource TIMER = () -> {
+		return Timer.getFPGATimestamp();
+	};
+	static final Follower.Motor MOTOR_LEFT = (s) -> {
+		Robot.drive.setLeftMotor(s);
+	};
+	static final Follower.Motor MOTOR_RIGHT = (s) -> {
+		Robot.drive.setRightMotor(s);
+	};
+	static final Follower.DistanceSource ENCODER_LEFT = () -> {
+		return Robot.drive.getLeftDistance();
+	};
+	static final Follower.DistanceSource ENCODER_RIGHT = () -> {
+		return Robot.drive.getRightDistance();
+	};
 	
-	//For differentiation
-	//Since the trajectory always starts at distance = 0, the errors are always 0 initially
-	double lastTime, leftLastErr = 0, rightLastErr = 0;
+	Follower follower;
 	
     public FollowTrajectory(TankDriveTrajectory trajectory) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.drive);
-    	this.trajectory = trajectory;
+    	follower = new TankFollower(trajectory, MOTOR_LEFT, MOTOR_RIGHT, ENCODER_LEFT, ENCODER_RIGHT, TIMER,
+    			kV, kA, kP, kD);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.drive.resetEncoders();
-    	lastTime = Timer.getFPGATimestamp();
+    	follower.initialize();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	//Time difference
-    	double dt = Timer.getFPGATimestamp() - lastTime;
-    	double t = timeSinceInitialized();
-    	
-    	//Retrieve the data for the current time
-    	TankDriveMoment m = trajectory.get(t);
-    	//Left and right errors
-    	double leftErr = m.getLeftPosition() - Robot.drive.getLeftDistance();
-    	double rightErr = m.getRightPosition() - Robot.drive.getRightDistance();
-    	//Calculate left and right position derivatives
-    	//The desired velocity is subtracted to get the difference
-    	double leftDeriv = (leftErr - leftLastErr) / dt 
-    			- m.getLeftVelocity();
-    	double rightDeriv = (rightErr - rightLastErr) / dt
-    			- m.getRightVelocity();
-    	//Calculate motor outputs
-    	double leftOutput = kA * m.getLeftAcceleration() + kV * m.getLeftVelocity()
-    			+ kP * leftErr + kD * leftDeriv;
-    	double rightOutput = kA * m.getRightAcceleration() + kV * m.getRightVelocity()
-    			+ kP * rightErr + kD * rightDeriv;
-    	//Constrain
-    	leftOutput = Math.max(-1, Math.min(1, leftOutput));
-    	rightOutput = Math.max(-1, Math.min(1, rightOutput));
-    	
-    	Robot.drive.setMotorsVBus(leftOutput, rightOutput);
-    	
-    	lastTime = Timer.getFPGATimestamp();
-    	leftLastErr = leftErr;
-    	rightLastErr = rightErr;
+    	follower.run();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return timeSinceInitialized() > trajectory.totalTime();
+        return !follower.isRunning();
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.drive.setMotorsVBus(0, 0);
+    	follower.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	end();
+    	follower.stop();
     }
 }
